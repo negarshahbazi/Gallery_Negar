a3c6d4cf8ab30731d960dc2dca5ad1c5ceb8d7cf
john Doe <negarshahbazi.official@gmail.com>
2024-04-01 15:28:28 +0200

    eeee

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Paint;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        // Récupérer les informations sur la peinture depuis la base de données
+
+        $paintRepository = $this->entityManager->getRepository(Paint::class);
+        $paints = $paintRepository->findAll(); // Supposons que l'ID de la peinture soit 1
+        foreach ($paints as $paint) {
+            // Récupérer gradeCount et gradeTotal de la peinture
+            $title = $paint->getTitle();
+            $photo = $paint->getPhoto();
+            $sizeW = $paint->getSizeW();
+            $sizeH = $paint->getSizeH();
+            $sizeD = $paint->getSizeD();
+            $price = $paint->getPrice();
+            $typeOfWork = $paint->getTypeOfWork();
+            $category = $paint->getCategory();
+            $status = $paint->getStatus();
+            $gradeCount = $paint->getGradeCount();
+            $gradeTotal = $paint->getGradeTotal();
+        }
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'title' => $title,
+            'photo' => $photo,
+            'sizeW' => $sizeW,
+            'sizeH' => $sizeH,
+            'sizeD' => $sizeD,
+            'price' => $price,
+            'typeOfWork' => $typeOfWork,
+            'category' => $category,
+            'status' => $status,
+            'gradeCount' => $gradeCount,
+            'gradeTotal' => $gradeTotal,
+
         ]);
     }
 }
---
+++ src/Form/PaintType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Paint;
+use App\Entity\Photo;
+use App\Entity\Status;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PaintType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('sizeW')
+            ->add('sizeH')
+            ->add('sizeD')
+            ->add('price')
+            ->add('typeOfWork')
+            ->add('gradeCount')
+            ->add('gradeTotal')
+            ->add('photo', EntityType::class, [
+                'class' => Photo::class,
+                'choice_label' => 'id',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Paint::class,
+        ]);
+    }
+}
